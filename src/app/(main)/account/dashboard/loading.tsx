9833ba6ff9240c02b33eaef9e7c896319133acde
alexander <alexander_pere@hotmail.com>
2025-03-12 12:02:56 -0500

    avances

--- src/app/(main)/account/dashboard/loading.tsx
+++ src/app/(main)/account/dashboard/loading.tsx
@@ -1,7 +1,15 @@
+import { ShoppingBag } from 'lucide-react'
+
 export default function Loading() {
   return (
-    <div className="text-ui-fg-base flex h-full w-full items-center justify-center">
-      cargandooo...{' '}
+    <div className="flex h-full flex-col items-center justify-center">
+      <div className="relative">
+        <div className="border-t-primary border-r-primary border-b-primary/30 border-l-primary/30 animate-spin rounded-full border-4" />
+
+        <div className="absolute inset-0 flex items-center justify-center">
+          <ShoppingBag className="text-primary animate-pulse" />
+        </div>
+      </div>
     </div>
   )
 }
